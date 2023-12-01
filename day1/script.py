ef0e209f24b41b0c9e7d01c8e46875ea2eb79a11
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-12-01 17:38:47 +0100

    Day 1-2: change algorithm

--- day1/script.py
+++ day1/script.py
@@ -1,9 +1,33 @@
-sum = 0
+def seperate_line(line: str):
+    """Seperate a string into an array of strings of letters or a digit"""
+    word = ''
+    arr = []
+    for c in line:
+        if not c.isnumeric():
+            if c == '\n':
+                arr.append(word)
+            word += c
+                
+        else:
+            if word:
+                arr.append(word)
+                word = ''
+            arr.append(c)
+    return arr
 
-with open('./data.txt') as f:
+
+sum = 0
+valid_strings = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+with open('./data_ex.txt') as f:
     for line in f.readlines():
-        numbers = [c for c in line if c.isnumeric()]
-        # print(numbers[0] + numbers[-1])
-        sum += (int)(numbers[0] + numbers[-1])
+        seperated = seperate_line(line)
+        print(seperated)
 
 print(sum)
+
+# Seperate line into array of "words" and digits
+# Find the first and last word or digit that is valid
+
+
+            
